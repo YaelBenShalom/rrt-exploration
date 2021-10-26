9ffefc7805272f7dfb3da2f96d2266cb6ad94813
Yael Ben Shalom <yael.bshalom@gmail.com>
2021-10-26 12:59:57 -0500

    reformat files

--- README.md
+++ README.md
@@ -1,24 +1,28 @@
 # MRS Hackathon
+
 GitHub repository - https://github.com/YaelBenShalom/RRT-Challenge
 
 ## Overview
+
 A Rapidly-Exploring Random Tree (RRT) is a fundamental path planning algorithm in robotics, first developed by Steven LaValle in 1998.
 Path planning is the task of moving a robot from one location to another, while avoiding obstacles and satisfying constraints.
 
 ### Task 1: Simple RRT
-Simple RRT implementation in a two-dimensional domain, *D=[0,100]X[0,100]*
 
+Simple RRT implementation in a two-dimensional domain, _D=[0,100]X[0,100]_
 
 ### Task 2: Planning a Path with Obstacles
-RRT implementation in a two-dimensional domain, *D=[0,100]X[0,100]*, with circular obstacles.
 
+RRT implementation in a two-dimensional domain, _D=[0,100]X[0,100]_, with circular obstacles.
 
 ## Usage and Configuration Instructions
+
 To generate the RRT implementation, run the code `random_tree.py`.
+
 - The number of obstacles can be changed from within the code.
 
 An RRT implementation without obstacles:
-![Tast_1](https://github.com/YaelBenShalom/RRT-Challenge/blob/master/images/Task_1(3).png)
+![Tast_1](<https://github.com/YaelBenShalom/RRT-Challenge/blob/master/images/Task_1(3).png>)
 
 An RRT implementation with 20 obstacles:
-![Tast_2](https://github.com/YaelBenShalom/RRT-Challenge/blob/master/images/Task_2(5).png)
+![Tast_2](<https://github.com/YaelBenShalom/RRT-Challenge/blob/master/images/Task_2(5).png>)
--- code/random_tree.py
+++ code/random_tree.py
@@ -1,7 +1,9 @@
-import matplotlib.pyplot as plt
 import random
 import math
 import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.patches
+from matplotlib.patches import Circle
 
 
 class RandomTree:
@@ -18,33 +20,54 @@ class RandomTree:
         self.q_goal = []
         self.number_of_obstacles = 20
         self.obstacles = []
+        self.is_path = True
 
     def RRT_generator(self):
         self.generate_obstacles(self.number_of_obstacles)
         self.random_q_init_and_goal()
-        self.G = [self.q_init]  # Adding the initial configuration to the RRT list
+        print(f"q_init: {self.q_init}")
+        print(f"q_goal: {self.q_goal}")
+
+        # Adding the initial configuration to the RRT list
+        self.G = [self.q_init]
         # plt.figure()
         plt.axis([0, self.D[0], 0, self.D[1]])
-        plt.title(f'An RRT with {self.number_of_obstacles} Obstacles and {self.K} Iterations')
-        plt.scatter(self.q_init[0], self.q_init[1], color='green', s=3)
-        # plt.scatter(self.q_goal[0], self.q_goal[1], color='green', s=3)
+        plt.title(
+            f'An RRT with {self.number_of_obstacles} Obstacles and {self.K} Iterations')
+        plt.scatter(self.q_init[0], self.q_init[1], color='black', s=10)
+        plt.scatter(self.q_goal[0], self.q_goal[1], color='black', s=10)
         for k in range(self.K):
-            # print(k)
+            print(k)
             self.q_rand = self.random_config(self.D)
             self.q_near = self.nearest_vertex(self.q_rand, self.G)
             self.q_new = self.new_config(self.q_near, self.q_rand, self.delta)
             if not self.check_collision(self.q_new):
                 self.G.append(self.q_new)
-                # plt.pause(1e-5)
-                plt.plot([self.q_near[0], self.q_new[0]], [self.q_near[1], self.q_new[1]], color='blue', markersize=1)
+                plt.pause(0.01)
+                plt.plot([self.q_near[0], self.q_new[0]], [
+                         self.q_near[1], self.q_new[1]], color='blue', markersize=1)
                 plt.scatter(self.q_new[0], self.q_new[1], color='black', s=1)
+
+                if not self.check_collision_line(self.q_new, self.q_goal):
+                    self.G.append(self.q_goal)
+                    plt.pause(0.01)
+                    # import ipdb; ipdb.set_trace()
+                    plt.plot([self.q_new[0], self.q_goal[0]], [
+                             self.q_new[1], self.q_goal[1]], color='blue', markersize=1)
+                    print(self.q_goal)
+                    print(self.q_new)
+                    print("Done!")
+                    plt.pause(0.01)
+                    break
         plt.show()
 
     def random_config(self, D):  # Generates a random position in the domain
-        self.q_rand = [random.randrange(self.D[0]), random.randrange(self.D[1])]
+        self.q_rand = [random.randrange(
+            self.D[0]), random.randrange(self.D[1])]
         return self.q_rand
 
-    def nearest_vertex(self, q_rand, G):  # Finds the vertex in  that is closest to the given position
+    # Finds the vertex in  that is closest to the given position
+    def nearest_vertex(self, q_rand, G):
         smallest_dist = 100
         for i in self.G:
             if self.distance(i, self.q_rand) < smallest_dist:
@@ -52,7 +75,8 @@ class RandomTree:
                 self.q_near = i
         return self.q_near
 
-    def new_config(self, q_near, q_rand, delta):  # Generates a new configuration in the tree
+    # Generates a new configuration in the tree
+    def new_config(self, q_near, q_rand, delta):
         dist = self.distance(self.q_near, self.q_rand)
         if dist != 0:
             ratio_dist = self.delta / dist
@@ -61,7 +85,13 @@ class RandomTree:
         return self.q_new
 
     def distance(self, point1, point2):
-        dist = math.sqrt(pow(point1[0] - point2[0], 2) + pow(point1[1] - point2[1], 2))
+        dist = math.sqrt(
+            pow(point1[0] - point2[0], 2) + pow(point1[1] - point2[1], 2))
+        return dist
+
+    def distance_to_line(self, point1, point2, point3):
+        dist = abs((point3[0] - point2[0]) * (point2[1] - point1[1]) - (point2[0] - point1[0]) * (
+            point3[1] - point2[1])) / math.sqrt(pow(point3[0] - point2[0], 2) + pow(point3[1] - point2[1], 2))
         return dist
 
     def check_collision(self, point):
@@ -71,17 +101,29 @@ class RandomTree:
                 collision = True
         return collision
 
+    def check_collision_line(self, point1, point2):
+        collision = False
+        for i in self.obstacles:
+            if self.distance_to_line(i[0], point1, point2) < i[1]:
+                collision = True
+        return collision
+
     def random_q_init_and_goal(self):
-        self.q_init = [random.randrange(self.D[0]), random.randrange(self.D[1])]
-        self.q_goal = [random.randrange(self.D[0]), random.randrange(self.D[1])]
+        self.q_init = [random.randrange(
+            self.D[0]), random.randrange(self.D[1])]
+        self.q_goal = [random.randrange(
+            self.D[0]), random.randrange(self.D[1])]
         while self.check_collision(self.q_init):
-            self.q_init = [random.randrange(self.D[0]), random.randrange(self.D[1])]
+            self.q_init = [random.randrange(
+                self.D[0]), random.randrange(self.D[1])]
         while self.check_collision(self.q_goal):
-            self.q_goal = [random.randrange(self.D[0]), random.randrange(self.D[1])]
+            self.q_goal = [random.randrange(
+                self.D[0]), random.randrange(self.D[1])]
 
     def generate_obstacles(self, number_of_obstacles):
         while number_of_obstacles:
-            circle_center = (random.randrange(self.D[0]), random.randrange(self.D[1]))
+            circle_center = (random.randrange(
+                self.D[0]), random.randrange(self.D[1]))
             circle_radius = random.randrange(10)
             circle = plt.Circle(circle_center, circle_radius, color='r')
             plt.gcf().gca().add_artist(circle)
@@ -90,7 +132,7 @@ class RandomTree:
 
 
 def main():
-    random_tree = RandomTree(1500, 1, [100, 100])
+    random_tree = RandomTree(1500, 2, [100, 100])
     random_tree.RRT_generator()
     # print(random_tree)
 
